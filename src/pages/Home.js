ed44643a5653a06b77b987d920c66ebdc683e208
DESKTOP-P7TDC68\Sujin <resqpa79@gmail.com>
2022-02-27 10:56:46 +0900

    기본 화면 변경

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import react from 'react';
+
+function Home() {
+    return (
+        <>
+            <header><h2>Web - Game - Demo</h2></header>
+            <div>
+                <button><a>Test 1 START</a></button>
+            </div>
+        </>
+    );
+}
+
+export default Home;
